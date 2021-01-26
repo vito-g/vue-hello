8f92f3937bb036a02d44305960d12af71475040b
Vito Gentile <gentilevito1076@gmail.com>
2021-01-26 13:49:43 +0100

    STAMPA a Schermo Titolo

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*TRACCIA
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Aggiungere un’immagine presa anch’essa da un data
+Fatto questo, (bonus) sbizzarritevi. Provate un po' di aspetti che abbiamo già sperimentato oggi in classe, con eventi e metodi. E' importantissimo iniziare a prendere confidenza non solo con Vue.js ma anche con la sua documentazione e provare a raccimolare informazioni anche da lì. https://vuejs.org/v2/guide/index.html
+Ma ricordate: se volete sperimentare non andate oltre a ciò che abbiamo visto in classe oggi.
+v-model
+v-bind
+v-on
+methods*/
+
+var app = new Vue({
+ el: '#root',
+ data: {
+   title : 'IT\'S THE FIRST VEU EXPERIENCE'
+ }
+});
+
+
+
+
+
+
+
+
+
+
+Vue.config.devtools = true;
